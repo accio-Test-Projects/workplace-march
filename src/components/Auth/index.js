0efd0a1942d7519368a0232f059e30a1cf41b37d
sahil jaryal <sahil199926@gmail.com>
2023-03-14 21:54:50 +0530

    auth flow

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -1,9 +1,37 @@
-import React from 'react'
-function Auth({usertype}) {
-
+import React from "react";
+import btnIcon from "../../assects/btn.png";
+import "./auth.css";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "../../firebaseconfig";
+function Auth({ usertype }) {
+  const provider = new GoogleAuthProvider();
+  const signIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const {user} = result ;
+        const { email, displayName, photoURL } = user;
+        console.log(email, displayName, photoURL, "user");
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        console.log(error, "error");
+      });
+  };
   return (
-    <div>Auth for {usertype} </div>
-  )
+    <div className="auth-container">
+      <div>
+        <h1>Welcome {usertype}!!</h1>
+        <h3>Please Sign IN</h3>
+
+        <div>
+          <button onClick={signIn}>
+            <img src={btnIcon} alt="btn" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Auth
+export default Auth;
